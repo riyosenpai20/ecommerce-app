75af5ace9fe7c486c4e9be0b117fb0c95413f3c6
riyo <riyosenpai20@gmail.com>
2023-01-26 21:25:36 +0700

    build ecommerce app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import {BsSearch} from 'react-icons/bs';
+import {
+  BsSearch, 
+  BsFillHeartFill, 
+  BsArrowRepeat,
+  BsFillPersonFill,
+  BsFillBasketFill,
+} from 'react-icons/bs';
 
 const Header = () => {
   return (
@@ -21,10 +27,10 @@ const Header = () => {
       </header>
       <header className="header-upper py-3">
         <div className="container-xxl">
-          <div className="row">
+          <div className="row align-items-center">
             <div className="col-2">
               <h2>
-                <Link>
+                <Link className='text-white'>
                   Store Name 
                 </Link>
               </h2>
@@ -38,12 +44,50 @@ const Header = () => {
                   arial-label="Serach Product Here ..." 
                   aria-describedby="basic-addon2"
                 />
-                <span className='input-group-text' id='basic-addon2'>
+                <span className='input-group-text px-3' id='basic-addon2'> 
                   <BsSearch />
                 </span>
               </div>
             </div>
-            <div className='col-5'></div>
+            <div className='col-5'>
+              <div className='header-upper-links d-flex align-items-center justify-content-between'>
+                <div>
+                  <Link className='text-white'>
+                    <div>
+                      <BsArrowRepeat className='fs-1'/>
+                    </div>
+                    <p>Compare <br/> Products</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='text-white'>
+                  <div>
+                      <BsFillHeartFill className='fs-1'/>
+                    </div>
+                    <p>Favorite <br/> Wishlist</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='text-white'>
+                    <div>
+                      <BsFillPersonFill className='fs-1'/>
+                    </div>
+                    <p>Log In <br /> My Account</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='text-white d-flex align-items-center gap-20'>
+                    <div className=''>
+                      <BsFillBasketFill className='fs-1'/>
+                    </div>
+                    <div className='d-flex flex-column gap-10'>
+                      <span className='badge bg-white text-dark'>0</span>
+                      <p>$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
