b290a772365618a7da37255efc52befd7a1848e0
riyo <riyosenpai20@gmail.com>
2023-01-27 23:38:40 +0700

    build ecommerce app

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
       </header>
       <header className="header-upper py-3">
         <div className="container-xxl">
-          <div className="row align-items-center">
+          <div className="row align-items-center d-flex">
             <div className="col-2">
               <h2>
                 <Link className='text-white'>
@@ -36,7 +36,7 @@ const Header = () => {
               </h2>
             </div>
             <div className='col-5'>
-              <div className='input-group mb-3'>
+              <div className='input-group align-self-center'>
                 <input 
                   type="text" 
                   className='form-control' 
@@ -52,39 +52,72 @@ const Header = () => {
             <div className='col-5'>
               <div className='header-upper-links d-flex align-items-center justify-content-between'>
                 <div>
-                  <Link className='text-white'>
+                  <Link className='text-white d-flex flex-row column-gap-2 align-items-center'>
                     <div>
                       <BsArrowRepeat className='fs-1'/>
                     </div>
-                    <p>Compare <br/> Products</p>
+                    <p className='mb-0'>Compare <br/> Products</p>
                   </Link>
                 </div>
                 <div>
-                  <Link className='text-white'>
+                  <Link className='text-white d-flex flex-row column-gap-2 align-items-center'>
                   <div>
                       <BsFillHeartFill className='fs-1'/>
                     </div>
-                    <p>Favorite <br/> Wishlist</p>
+                    <p className='mb-0'>Favorite <br/> Wishlist</p>
                   </Link>
                 </div>
                 <div>
-                  <Link className='text-white'>
+                  <Link className='text-white d-flex flex-row column-gap-2 align-items-center'>
                     <div>
                       <BsFillPersonFill className='fs-1'/>
                     </div>
-                    <p>Log In <br /> My Account</p>
+                    <p className='mb-0'>Log In <br /> My Account</p>
                   </Link>
                 </div>
                 <div>
-                  <Link className='text-white d-flex align-items-center gap-20'>
+                  <Link className='text-white d-flex flex-row column-gap-2 align-items-center'>
                     <div className=''>
                       <BsFillBasketFill className='fs-1'/>
                     </div>
-                    <div className='d-flex flex-column gap-10'>
+                    <div className='d-flex flex-column gap-1'>
                       <span className='badge bg-white text-dark'>0</span>
-                      <p>$ 500</p>
+                      <p className='mb-0'>$ 500</p>
                     </div>
                   </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className='header-bottom py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='menu-bottom d-flex align-items-center gap-30'>
+                <div>
+                <div class="dropdown">
+                  <button class="btn btn-secondary dropdown-toggle bg-transparent border-0 align-items-center d-flex gap-15" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="./menu.svg" className='iconMenu' alt="" />
+                    <span className='me-5 d-inline-block'>
+                      Shop Categories
+                    </span>
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </div>
+                </div>
+                <div className='menu-links'>
+                  <div className='d-flex align-items-center gap-15'>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/">Blogs</NavLink>
+                    <NavLink to="/">Contact</NavLink>
+                  </div>
                 </div>
               </div>
             </div>
