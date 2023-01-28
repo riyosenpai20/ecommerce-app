b7557320da1f1e9a1729054b7d34ce7ad58536fa
riyo <riyosenpai20@gmail.com>
2023-01-28 22:19:15 +0700

    build ecommerce app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <>
       <section className='home-wrapper-1 py-5'>
         <div className='container-xxl'>
-          <div className='row'>
+          <div className=' row'>
             <div className='col-6'>
-              <div className='main-banner-content p-3'>
-                 <img src="https://images.unsplash.com/photo-1596207498818-edb80522f50b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" width="700px" alt="" />
-                 <div className='main-banner-content'>
-
+              <div className='main-banner position-relative p-3'>
+                 <img src="https://images.unsplash.com/photo-1596207498818-edb80522f50b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" width="700px" className='img-fluid rounded-3' alt="" />
+                 <div className='main-banner-content position-absolute'>
+                  <h4 className='text-uppercase banner-desc'>Supercharged For Pros</h4>
+                  <h5 className='banner-desc'>iPad S13+ Pro.</h5>
+                  <p>From $999.00 or $41.62/mo.</p>
+                  <Link className='text-uppercase button-banner'>Buy Now</Link>
                  </div>
               </div>
             </div>
             <div className='col-6'>
-
+            <div className='d-flex flex-warp justify-content-between align-items-center'>
+              <div className='small-banner position-relative p-3'>
+                <img src="https://images.unsplash.com/photo-1596207498818-edb80522f50b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" width="50%" className='img-fluid rounded-3' alt="" />
+                <div className='small-banner-content position-absolute'>
+                  <h4>Supercharged For Pros.</h4>
+                  <h5>Ipad S13+ Pro.</h5>
+                  <p>From $999.00 or $50.00/month</p>
+                  <Link className='button-banner'>Buy Now</Link>
+                </div>
+              </div>
+            </div>
             </div>
           </div>
         </div>
