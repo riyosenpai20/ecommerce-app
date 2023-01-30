aa2d4fd41045f5db2bb570502d89674803222431
riyo <riyosenpai20@gmail.com>
2023-01-30 20:51:48 +0700

    build ecommerce app

---
+++ src/components/BlogCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const BlogCard = () => {
+  return (
+    <div className='col-3'>
+        <div className="blog-card">
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1542751110-97427bbecf20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1984&q=80" className='img-fluid' alt="" />
+            </div>
+            <div className="blog-content">
+                <p className="date">30 Jan, 2023</p>
+                <h5 className="title">
+                    So Apple HAS Thought About Making Even Larger iPad Pros
+                </h5>
+                <p className='desc'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolorum aliquam ducimus ea obcaecati
+                </p>
+                <Link to={'/'} className="button">
+                    Read more
+                </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BlogCard
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {BsCreditCard, BsFillGiftFill, BsHeadset, BsPercent, BsTruck} from 'react-icons/bs'
-import Marquee from 'react-fast-marquee'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {BsCreditCard, BsFillGiftFill, BsHeadset, BsPercent, BsTruck} from 'react-icons/bs';
+import Marquee from 'react-fast-marquee';
+import BlogCard from '../components/BlogCard';
 
 const Home = () => {
   return (
@@ -200,6 +201,19 @@ const Home = () => {
                 </Marquee>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className='blog-wrapper py-5 home-wrapper'>
+        <div className="container-xxl">
+          <div className="row">
+            <div className='col-12'>
+              <h3 className='section-heading'>Our Latest Blogs</h3>
+            </div>
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
           </div>
         </div>
       </section>
