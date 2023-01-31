c81e17f49725fcb8a794efabb9e1657bce6cd65c
riyo <riyosenpai20@gmail.com>
2023-01-31 16:44:46 +0700

    add featured collection

---
+++ src/components/ProductCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div className='col-3'>
+        <div className="product-card">
+            <div className="product-image">
+                <img src="./images/smart-watch.png" alt="" />
+            </div>
+            <div className="product-details">
+                <h6>Havels</h6>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductCard
