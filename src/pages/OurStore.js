cdaef74c9fa2e7c9982d987874a39d24839a9927
riyo <riyosenpai20@gmail.com>
2023-02-06 22:44:33 +0700

    build ourstore page

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,10 +1,138 @@
 import React from 'react'
 import Breadcrumb from '../components/Breadcrumb'
+import Meta from '../components/Meta'
 
 const OurStore = () => {
   return (
     <>
+      <Meta title={"Our Store"} />
       <Breadcrumb title="Our Store" />
+      <div className="store-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className='filter-card mb-3'>
+                <h3 className="filter-title">Shop By Categories</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li>Watch</li>
+                    <li>TV</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+              <div className='filter-card mb-3'>
+                <h3 className="filter-title">Filter By</h3>
+                <div>
+                  <h5 className="sub-title">Availability</h5>
+                  <div>
+                    <div className="form-check">
+                      <input type="checkbox" className='form-check-input' id="inStock" />
+                      <label className='form-check-label' htmlFor='inStock' >
+                        In Stock (1)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input type="checkbox" className='form-check-input' id="OFS" />
+                      <label className='form-check-label' htmlFor='OFS' >
+                        Out of Stock
+                      </label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Price</h5>
+                  <div className="d-flex align-items-center gap-10">
+                    <div className="form-floating mb-3">
+                      <input 
+                        type="text" 
+                        name="" 
+                        id="floatingInput" 
+                        className="form-control form-control-sm"
+                        placeholder='From' />
+                        <label htmlFor="floatingInput">From</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                      <input 
+                        type="text" 
+                        name="" 
+                        id="floatingInput" 
+                        className="form-control form-control-sm"
+                        placeholder='To' />
+                        <label htmlFor="floatingInput">To</label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Colors</h5>
+                  <div>
+                    <div className='d-flex flex-wrap'>
+                      <ul className="colors ps-0">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Size</h5>
+                  <div>
+                    <div className="form-check">
+                      <input 
+                        type="checkbox" 
+                        className='form-check-input'
+                        name="" 
+                        id="size-1" 
+                      />
+                      <label className='form-check-label' htmlFor="size-1">
+                        S(2)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input 
+                        type="checkbox" 
+                        className='form-check-input'
+                        name="" 
+                        id="size-2" 
+                      />
+                      <label className='form-check-label' htmlFor="size-2">
+                        M(2)
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='filter-card mb-3'>
+                <h3 className="filter-title">Product tags</h3>
+                <div>
+                  <div className="product-tags d-flex flex-wrap align-items-center gap-10">
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Headphone</span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Laptop</span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Camera</span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">Smartphone</span>
+                  </div>
+                </div>
+              </div>
+              <div className='filter-card mb-3'>
+                <h3 className="filter-title">Random Product</h3>
+                <div>
+                  <div className="random-products d-flex">
+                    <div className="w-50">
+                      <img src="./images/smart-watch.png" alt="smartwatch" width={"80%"} className="img-fluid" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-9"></div>
+          </div> 
+        </div>
+      </div>
     </>
   )
 }
