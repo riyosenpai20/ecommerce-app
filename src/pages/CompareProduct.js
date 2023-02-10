168aea19e126e09c5a1c580c15d9ebbb1e4459e0
riyo <riyosenpai20@gmail.com>
2023-02-10 23:50:07 +0700

    build compare product page

--- src/pages/CompareProduct.js
+++ src/pages/CompareProduct.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import Breadcrumb from '../components/Breadcrumb';
+import Meta from '../components/Meta';
+import {
+  BsSearch, 
+  BsFillHeartFill, 
+  BsArrowRepeat,
+  BsFillPersonFill,
+  BsCart4,
+} from 'react-icons/bs';
 
 const CompareProduct = () => {
   return (
-    <div>CompareProduct</div>
+    <>
+      <Meta title={"Compare Products"} />
+      <Breadcrumb title={"Compare Products"}/>
+      <div className="compare-product-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="compare-product-card position-relative">
+                <div>
+                  <BsArrowRepeat />
+                </div>
+                <div className="product-card-image">
+                  <img src="./images/smart-watch.png" alt="watch" width="60%" />
+                </div>
+                <div className="compare-product-details">
+                  <h5 className="title">
+                    Apple Smartwatch series 2
+                  </h5>
+                  <h6 className='price'>$400</h6>
+                  <div>
+                    <div className="product-details">
+                      <h5>Brand:</h5>
+                      <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
