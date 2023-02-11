52ed91984fb0918cacada4e2cf73b151b75a5764
riyo <riyosenpai20@gmail.com>
2023-02-11 22:17:25 +0700

    build wishlist page

---
+++ src/pages/Wishlist.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Breadcrumb from '../components/Breadcrumb';
+import Meta from '../components/Meta';
+
+const Wishlist = () => {
+  return (
+    <>
+      <Meta title={"Favorite Wishlist"} />
+      <Breadcrumb title={"Favorite Wishlist"}/>
+
+    </>
+  )
+}
+
+export default Wishlist
